babd1af028d0b71d97f67279d35460f86ff4a202
Pat <patrick.h.needham@gmail.com>
2018-08-08 16:29:22 -0400

    creating root component

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 class App extends React.Component {
 
+  const DUMMY_DATA = [
+    {
+      senderId: "mookie_betts",
+      text: "who'll win?"
+    },
+    {
+      senderId: "jaylen_brown",
+      text: "who'll win?"
+    }
+  ]
+
+  constructor() {
+    super()
+    this.state = {
+       messages: DUMMY_DATA
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <Title />
-        <MessageList />
+        <MessageList messages={this.state.messages} />
         <SendMessageForm />
       </div>
     )
