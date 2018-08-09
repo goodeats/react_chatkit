cda05d552670ed156ad9d79b0e35aec15dbf58b2
Pat <patrick.h.needham@gmail.com>
2018-08-09 14:47:50 -0400

    need these dependencies to run react

--- index.js
+++ index.js
@@ -1,51 +1,6 @@
-const DUMMY_DATA = [
-  {
-    senderId: "mookie_betts",
-    text: "who'll win?"
-  },
-  {
-    senderId: "jaylen_brown",
-    text: "who'll win?"
-  }
-];
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-class App extends React.Component {
+import App from './App';
 
-  constructor() {
-    super();
-    this.state = {
-       messages: DUMMY_DATA
-    };
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <Title />
-        <MessageList messages={this.state.messages} />
-        <SendMessageForm />
-      </div>
-    );
-  }
-}
-
-class MessageList extends React.Component {
-  render() {
-    return (
-      <ul className="message-list">
-        {this.props.messages.map(message => {
-          return (
-           <li key={message.id}>
-             <div>
-               {message.senderId}
-             </div>
-             <div>
-               {message.text}
-             </div>
-           </li>
-         );
-       })}
-     </ul>
-   );
-  }
-}
+ReactDOM.render(<App />, document.getElementById('root'));
