c3915f951158ad98596cc6775a966c11e7841a72
Pat <patrick.h.needham@gmail.com>
2018-08-09 16:25:56 -0400

    changed message to a functional component

--- components/Message.js
+++ components/Message.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
-class Message extends React.Component {
-    render() {
-        return (
-            <div className="message">
-              <div className="message-username">{this.props.username}</div>
-              <div className="message-text">{this.props.text}</div>
-            </div>
-        )
-    }
+// functional component
+// good if no state, lifecycle, or other methods
+function Message(props) {
+  return (
+      <div className="message">
+        <div className="message-username">{props.username}</div>
+        <div className="message-text">{props.text}</div>
+      </div>
+  )
 }
+
+// dumb component
+// class Message extends React.Component {
+//     render() {
+//         return (
+//             <div className="message">
+//               <div className="message-username">{this.props.username}</div>
+//               <div className="message-text">{this.props.text}</div>
+//             </div>
+//         )
+//     }
+// }
 
 export default Message
