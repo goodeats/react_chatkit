40b285d0e97b4c8da6cd69e9d4cfbe0fcf5e99e3
Pat <patrick.h.needham@gmail.com>
2018-08-09 21:11:05 -0400

    auto scroll with ux; create a new room

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,34 +1,49 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Message from './Message'
 
 const DUMMY_DATA = [
-    {
-        senderId: 'perborgen',
-        text: 'Hey, how is it going?'
-    },
-    {
-        senderId: 'janedoe',
-        text: 'Great! How about you?'
-    },
-    {
-        senderId: 'perborgen',
-        text: 'Good to hear! I am great as well'
-    }
+  {
+    senderId: 'perborgen',
+    text: 'Hey, how is it going?'
+  },
+  {
+    senderId: 'janedoe',
+    text: 'Great! How about you?'
+  },
+  {
+    senderId: 'perborgen',
+    text: 'Good to hear! I am great as well'
+  }
 ]
 
 class MessageList extends React.Component {
-    render() {
-        return (
-            <div className="message-list">
-              {/* {DUMMY_DATA.map((message, index) => { */}
-              {this.props.messages.map((message, index) => {
-                return (
-                  <Message key={index} username={message.senderId} text={message.text} />
-                )
-              })}
-            </div>
-        )
+
+  componentWillUpdate() {
+    const node = ReactDOM.findDOMNode(this)
+    this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+  }
+
+  // react lifecycle method
+  componentDidUpdate(){
+    if (this.shouldScrollToBottom){
+      const node = ReactDOM.findDOMNode(this)
+      node.scrollTop = node.scrollHeight
     }
+  }
+
+  render() {
+    return (
+      <div className="message-list">
+        {/* {DUMMY_DATA.map((message, index) => { */}
+        {this.props.messages.map((message, index) => {
+          return (
+            <Message key={index} username={message.senderId} text={message.text} />
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default MessageList
